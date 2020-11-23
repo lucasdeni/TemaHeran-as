79d83dbe1c110d5537f37946aad9771003b85354
Lucas Denipoti <lucasfdenipoti@gmail.com>
2020-11-23 20:01:08 -0300

    Adicionar arquivos de projeto.

---
+++ TemaHeranças/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TemaHeranças
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
