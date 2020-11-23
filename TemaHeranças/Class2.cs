ce661b04b946b30a88bd3059e26ba0696d62bee7
Lucas Denipoti <lucasfdenipoti@gmail.com>
2020-11-23 20:22:54 -0300

    código pronto

---
+++ TemaHeranças/Class2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Cryptography.X509Certificates;
+
+namespace TemaHeranças
+{
+    class ContaJuridica : Conta
+    {
+        public double EmprestimoLimite { get; set; } // Declaração das variáveis e uso do encapsulamento
+        public ContaJuridica()
+        {
+            // Criação do construtor com parâmetros utilizando o da Classe Conta
+        }
+        public ContaJuridica(int numero, string titular, double saldo, double emprestimoLimite) : base(numero, titular, saldo) // Criação do construtor com parâmetros utilizando o da Classe Conta
+        {
+            EmprestimoLimite = emprestimoLimite;
+        }
+        public void Emprestimo(double saldoTotal) // Realiza o Empréstimo
+        {
+            if (saldoTotal <= EmprestimoLimite)
+            {
+                _saldo += saldoTotal;
+            }
+        }
+
+    }
+}
--- TemaHeranças/Program.cs
+++ TemaHeranças/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace TemaHeranças
 {
@@ -6,7 +7,29 @@ namespace TemaHeranças
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Conta acc = new Conta(1001, "Goku", 0.0);
+            ContaJuridica bacc = new ContaJuridica(1002, "Goten", 0.0, 500.0);
+            // Conta para Conta Juridica
+            Conta acc1 = bacc;
+            Conta acc2 = new ContaJuridica(1003, "Trunks", 0.0, 200.00);
+            // Conta para Conta Poupança
+            Conta acc3 = new ContaPoupança(1004, "Vegeta", 0.0, 0.01);
+            ContaJuridica acc4 = (ContaJuridica)acc2; // Downcasting
+            acc4.Emprestimo(100.0);
+            // Verficar se a nova conta criada é uma Juridica ou Poupança
+            if (acc3 is ContaJuridica)
+            {
+                ContaJuridica acc5 = (ContaJuridica)acc3;
+                acc5.Emprestimo(200.0);
+                Console.WriteLine("Emprestimo!");
+            }
+            if (acc3 is ContaPoupança)
+            {
+                ContaPoupança acc5 = (ContaPoupança)acc3;
+                acc5.atualizacaoSaldo();
+                Console.WriteLine("Atualizado!");
+            }
+            Console.ReadKey();
         }
     }
 }
